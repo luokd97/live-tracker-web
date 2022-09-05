c598e2d3668ab7b0fbbf83c38fb1b583919805de
luokd97 <luokd97@gmail.com>
2022-09-06 01:27:06 +0800

    add refresh button

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 
 <script>
-const API_URL = `http://ratman.cc:8088/live/list`
+const API_URL = `/live/list`
 
 export default {
   data: () => ({
-    room_list: null,
+    room_list: [],
+    refreshing: false,
+    data: null,
+    total: 0,
     count: 0
   }),
 
@@ -17,21 +20,14 @@ export default {
   },
 
   methods: {
-    fetchData() {
-      let headers = new Headers();
-
-      console.log('fetchData() invoke');
-      const url = `${API_URL}`
-      this.room_list = ''
-      fetch(url, {
-        headers: headers
-      })
-        .then((response) => response.json())
-        .then((data) => this.room_list = data);
-    },
-    increment() {
-      this.count++
-      this.fetchData()
+    async fetchData() {
+      this.refreshing = true;
+      console.log('fetch data...');
+      var data = await (await fetch(API_URL)).json();
+      this.room_list = data.data.liveRoomResults
+      this.total = data.data.total
+      console.log("fetch success")
+      this.refreshing = false;
     }
 
   }
@@ -41,13 +37,17 @@ export default {
 <template>
   <!-- <button @click="increment">count is: {{ count }}</button> -->
   <h3>以下房间正在直播中</h3>
+
+  <button v-if="!refreshing" @:click="fetchData">刷新</button>
+  <p v-if="refreshing">正在查询...</p>
+
   <ul>
     <li v-for="{ roomStatus, uname, roomTitle, platform, url } in room_list">
       <a :href="url" target="_blank" class="commit">{{ uname }}</a>
       - <span class="message">{{ roomTitle }}</span><br>
     </li>
   </ul>
-  <p>总计（{{ room_list.length }}）</p>
+  <p>总计（{{ room_list.length }}/{{total}}）</p>
 </template>
 
 <style>
